d6b3bcf79cbfd3d5193b412b096c1749bb42b8f2
tsy2001 <tsy88123@outlook.com>
2026-01-29 21:51:24 +0800

    更改u8g2显示位置

--- user/app_u8g2.c
+++ user/app_u8g2.c
@@ -165,13 +165,13 @@ void u8g2_cdplayer(void)
         }
 
         u8g2_DrawGlyph(&u8g2, 0, 21, 484);  // 喇叭
-        u8g2_DrawGlyph(&u8g2, 45, 21, 567); // 专辑
+        u8g2_DrawGlyph(&u8g2, 42, 21, 567); // 专辑
 
-        u8g2_SetFont(&u8g2, u8g2_font_misans_thin_12_ascii);
+        u8g2_SetFont(&u8g2, u8g2_font_misans_thin_9_ascii);
         sprintf(oled_buffer, "%02d", (int)(volume*100));
-        u8g2_DrawStr(&u8g2, 23, 16, oled_buffer); // 音量
+        u8g2_DrawStr(&u8g2, 23, 15, oled_buffer); // 音量
         sprintf(oled_buffer, "%02d/%02d", now_tracks - 1, total_tracks);
-        u8g2_DrawStr(&u8g2, 67, 16, oled_buffer);
+        u8g2_DrawStr(&u8g2, 65, 15, oled_buffer);
         if (album_ready_flag && (artist_buf[0] != '\0' || title_buf[0] != '\0'))
         {
             // artist_buf是否为空，空则显示Unknown(todo：个别情况异常需修复)
